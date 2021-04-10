34712febf550621997fa3e1a4a1e22b1f7ad8c29
akash tiwari <akashtiwari370@gmail.com>
2021-04-10 12:41:43 +0530

    MidSem

---
+++ AKTReducer.java
@@ -0,0 +1,23 @@
+// Akash Tiwari
+// DS
+// University roll no- 2015146
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+
+public class AKTReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+		int y = 0;
+		for (IntWritable x : values) {
+			y += 1;
+		}
+		context.write(key, new IntWritable(y));
+	}
+}
